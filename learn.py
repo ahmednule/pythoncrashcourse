45083f1d162276aeddb54f5f6fe60aeb77d0a627
Rafikitaban <obuyadaisy278@gmail.com>
2024-06-13 14:33:14 +0300

    python practice

---
+++ learn.py
@@ -0,0 +1,18 @@
+# first comment
+# print ("Hello World")
+
+#lambda functions
+# multiply = lambda x, y: y* x
+# print(multiply (12, 7))
+
+#loop 
+# for i in range (3):
+#     print(f"i is 2.5")
+
+#while loop 
+# count = 0
+# while count <1:
+#     print (f"count is {count}")
+#     count =0
+
+
